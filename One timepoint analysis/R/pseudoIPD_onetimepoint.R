baf533bdf054892685b08ce569485df0cc67d903
Katerina Papadimitropoulou <katerina.papadimitropoulou@gmail.com>
2021-03-02 13:00:50 +0100

    Update pseudoIPD_onetimepoint.R

--- One timepoint analysis/R/pseudoIPD_onetimepoint.R
+++ One timepoint analysis/R/pseudoIPD_onetimepoint.R
@@ -13,10 +13,10 @@ data.long <- reshape(data.wide, direction='long',
                                              c('m1i', 'm2i'),
                                              c('sd1i', 'sd2i'),
                                              c('n1i', 'n2i')),
-                             timevar='group',
-                             times  = c(0,1),
-                             v.names= c('mean','sd','n'),
-                             idvar=  'study')
+                             timevar = 'group',
+                             times   = c(0,1),
+                             v.names = c('mean','sd','n'),
+                             idvar   = 'study')
 #-----------------------------------------------------------------------------------------------------------------------------
 
 # Generate IPD
